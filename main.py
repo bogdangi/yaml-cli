024a01b954a8f1991a0643026b6094ef928721ee
Bogdan Girman <bogdan.girman@gmail.com>
2018-09-04 11:15:34 +0200

    Add more cases

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
 import ruamel.yaml
 
 
+def update(obj, path, value):
+    if not path:
+        return obj
+
+    if path[0].isdigit():
+        path[0] = int(path[0])
+
+    if len(path) == 1:
+        obj[path[0]] = value
+        return obj
+    else:
+        new_type = dict
+        if type(path[0]) is int:
+            new_type = list
+
+        if new_type is dict:
+            obj[path[0]] = update(
+                obj.get(path[0], new_type()), path[1:], value)
+        else:
+            if len(obj) > path[0]:
+                obj[path[0]] = update(dict(), path[1:], value)
+            else:
+                obj.append(update(dict(), path[1:], value))
+
+        return obj
+
+
 def yaml(stream, set_string):
     doc = ruamel.yaml.load(
         stream,
@@ -8,16 +35,12 @@ def yaml(stream, set_string):
         preserve_quotes=True,
     )
 
-    path = set_string.split('.')
-    value = path[-1].split('=')[1]
-    path[-1] = path[-1].split('=')[0]
+    path = set_string.split('=')[0].split('.')
+    if type(path) is not list:
+        path = [path]
 
-    doc_update = {}
+    value = set_string.split('=')[1]
 
-    while path:
-        doc_update = { path.pop(): value }
-        value = doc_update
-
-    doc.update(doc_update)
+    doc = update(doc, path, value)
 
     return ruamel.yaml.dump(doc, Dumper=ruamel.yaml.RoundTripDumper)
--- tests/bdd/features/set.feature
+++ tests/bdd/features/set.feature
@@ -7,5 +7,9 @@ Scenario: Set a value in simple yaml file
     Then I should see the file with <content_after>
 
     Examples:
-        | content_before     | value         | content_after      |
+        | content_before     | value         | content_after        |
+        | name: bob\n        | name=ted      | name: ted\n          |
         | user:\n  name: bob | user.name=ted | user:\n  name: ted\n |
+        | a:\n- b\n- c\n     | a.0=a         | a:\n- a\n- c\n       |
+        | a:\n- b\n- c\n     | a.0.a=b       | a:\n- a: b\n- c\n    |
+        | a:\n- b\n          | a.1.c=b       | a:\n- b\n- c: b\n    |
